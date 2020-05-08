98d0cc8a4b54aef355671e86b440779f0d25a736
Elliot Masor <elliot.masor@tala.co>
2020-05-07 20:05:56 -0700

    fix sync issue in exception handler

--- src/test/groovy/co/tala/performance/async/ParallelsSpec.groovy
+++ src/test/groovy/co/tala/performance/async/ParallelsSpec.groovy
@@ -1,5 +1,6 @@
 package co.tala.performance.async
 
+import co.tala.performance.exception.AggregateException
 import org.codehaus.groovy.runtime.powerassert.PowerAssertionError
 import spock.lang.Specification
 
@@ -20,7 +21,7 @@ class ParallelsSpec extends Specification {
             boolean isTriggered = false
 
         when: "runAsync is invoke with a Closure to set the boolean to true"
-        and:"waitAll is invoked"
+        and: "waitAll is invoked"
             sut.runAsync { isTriggered = true }.waitAll()
 
         then: "the boolean value should be true"
@@ -56,21 +57,31 @@ class ParallelsSpec extends Specification {
     }
 
     def "waitAll should throw all exceptions that occur on all threads"() {
-        given: "4 threads are invoked, and 2 of them throw exceptions"
+        given: "several threads are invoked, and 5 of them throw exceptions"
             sut
-                    .runAsync { sleep(10) }
-                    .runAsync { sleep(10); throw new Exception("exception 1") }
-                    .runAsync { sleep(50) }
-                    .runAsync { sleep(10); throw new Exception("exception 2") }
-                    .runAsync { sleep(10) }
+                .runAsync { sleep(10) }
+                .runAsync { sleep(10); throw new Exception("exception 1") }
+                .runAsync { sleep(50) }
+                .runAsync { sleep(10); throw new Exception("exception 2") }
+                .runAsync { sleep(10) }
+                .runAsync { sleep(10); throw new Exception("exception 3") }
+                .runAsync { sleep(10) }
+                .runAsync { sleep(50) }
+                .runAsync { sleep(10); throw new Exception("exception 4") }
+                .runAsync { sleep(10) }
+                .runAsync { sleep(10); throw new Exception("exception 5") }
+                .runAsync { sleep(10) }
 
         when: "waitAll is invoked"
             sut.waitAll()
 
         then: "the exceptions should all be thrown"
-            // TODO figure why sometimes only 1 exception is thrown
-            PowerAssertionError e = thrown PowerAssertionError
-            e.message.contains("java.lang.Exception")
+            AggregateException e = thrown AggregateException
+            e.message.contains("exception 1")
+            e.message.contains("exception 2")
+            e.message.contains("exception 3")
+            e.message.contains("exception 4")
+            e.message.contains("exception 5")
 
     }
 }
